7f92dad074c92af15eaf9dbabae2f63c7443c04c
unknown <az0809j@gmail.com>
2026-05-02 22:25:29 -0400

    lista de items con sus caracteristicas agregadas

--- src/scripts/entities/item.lua
+++ src/scripts/entities/item.lua
@@ -1,36 +1,5 @@
 local item = {}
 
-item.TYPES = {
-    WEAPON = "weapon",
-    CONSUMIBLE = "consumible",
-    KEY_EVENT = "keyEvent"
-}
 
-function item.new(_id, _nameToShow, _texture, _type , _isStackable, _count, _hasWear, _levelOfWear, _onUse)
-
-    if not _isStackable then
-        _count = 0
-    end
-
-    if not _hasWear then
-        _levelOfWear = 0
-    end
-
-    local item = {
-        id = _id,
-        type = _type,
-        nameToShow = _nameToShow,
-        texture = _texture,
-        isStackable = _isStackable,
-        count = _count,
-        hasWear = _hasWear,
-        levelOfWear = _levelOfWear,
-        onUse = _onUse
-    }
-
-    item.texture:setFilter("nearest", "nearest")
-
-    return item
-end
 
 return item
---
+++ src/scripts/systems/itemsDefinition.lua
@@ -0,0 +1,116 @@
+local itemDefinitions = {
+
+    brokeBottle = {
+        name = "Pico de botella",
+        texture = "",
+        type = ITEM_TYPES.WEAPON,
+        isStackable = false,
+        count = 1,
+        haswear = true,
+        levelOfWear = 10,
+        onUse = function()
+            --atack
+        end
+    },
+
+    knife = {
+        name = "Cuchillo",
+        texture = "",
+        type = ITEM_TYPES.WEAPON,
+        isStackable = false,
+        count = 1,
+        haswear = true,
+        levelOfWear = 10,
+        onUse = function()
+            --atack
+        end
+    },
+
+    baseballBat = {
+        name = "Bate de baseball",
+        texture = "",
+        type = ITEM_TYPES.WEAPON,
+        isStackable = false,
+        count = 1,
+        haswear = true,
+        levelOfWear = 10,
+        onUse = function()
+            --atack
+        end
+    },
+
+    ironBar = {
+        name = "Palanca de hierro",
+        texture = "",
+        type = ITEM_TYPES.WEAPON,
+        isStackable = false,
+        count = 1,
+        haswear = true,
+        levelOfWear = 10,
+        onUse = function()
+            --atack
+        end
+    },
+
+    wrench = {
+        name = "Llave inglesa",
+        texture = "",
+        type = ITEM_TYPES.WEAPON,
+        isStackable = false,
+        count = 1,
+        haswear = true,
+        levelOfWear = 10,
+        onUse = function()
+            --atack
+        end
+    },
+
+    stones = {
+        name = "Piedras",
+        texture = "",
+        type = ITEM_TYPES.WEAPON,
+        isStackable = true,
+        count = 5,
+        haswear = false,
+        levelOfWear = 0,
+        onUse = function()
+            --throw
+        end
+    },
+
+    heavyStone = {
+        name = "Piedra",
+        texture = "",
+        type = ITEM_TYPES.CARRYABLE_OBJECT,
+        isStackable = false,
+        count = 1,
+        haswear = false,
+        levelOfWear = 0,
+        onUse = function()
+            --pick up
+        end
+    },
+
+    paperKey = {
+        name = "Código",
+        texture = "",
+        type = ITEM_TYPES.KEY_EVENT,
+        isStackable = false,
+        count = 1,
+        haswear = false,
+        levelOfWear = 0,
+        onUse = function()
+            --ver codigo
+        end
+    },
+
+}
+
+ITEM_TYPES = {
+    WEAPON = "weapon",
+    CONSUMIBLE = "consumible",
+    CARRYABLE_OBJECT = "carryableObject",
+    KEY_EVENT = "keyEvent"
+}
+
+return itemDefinitions
