5faae46359dd62a7778f64f28ea32e1970e92ae7
Ja8ier <az0809j@gmail.com>
2026-05-17 12:14:55 -0400

    MOD: Triggers visibles = no

--- src/scripts/states/stage1.lua
+++ src/scripts/states/stage1.lua
@@ -528,7 +528,7 @@ function stage1.draw()
         end
     end
 
-    cb.showBoxes(player, collisions, enemies, triggers, true)
+    cb.showBoxes(player, collisions, enemies, triggers, false)
     camera.ended()
 
     --frontground
